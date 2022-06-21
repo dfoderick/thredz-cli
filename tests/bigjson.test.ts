18f2414d92368a2fe4b3066a619ef2c3807b69ca
Dave Foderick <dfoderick@gmail.com>
2022-06-21 09:16:16 -0700

    fix test

--- tests/bigjson.test.ts
+++ tests/bigjson.test.ts
@@ -1,13 +1,12 @@
 import {Folder} from "../src/folder"
 import {Uploader} from "../src/uploader"
 import {Wallet} from "../src/wallet"
+import * as fs from 'fs-extra'
 
 test('write big json', async () => {
-  const wallet = Wallet.load(`
-    {
-      "keymeta":"xprv9s21ZrQH143K4aghKiKmqbBtbn1pgdgy4ZdKrwv4zGd4W41HzotWduzEyqN6HD7HjQpbjG9DjQzGprdxQCQmSwvmJQRUqCmEurPorvVVB3k","keyfunding":"xprv9s21ZrQH143K2GwnA1GB9CHuEST5bdc9dLMoj9PYNQAN6Apoid96xn4g64yryjUQ439GwAdCbvtvtRBEcwoGwYDr5NphGGFdiWraTcSHqfh",
-      "user":"test"
-    }`)
+  const jwallet = fs.readJSONSync('./.thredz')
+  jwallet.name = 'test'
+  const wallet = Wallet.load(JSON.stringify(jwallet))
     const folder = new Folder()
     folder.user = wallet.user
     const uploader = new Uploader(wallet, folder)
